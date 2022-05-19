70dff23af00b1c31ec35da9a60507b7fea290ef1
Gera! <g.zerbetto@gmail.com>
2022-05-19 17:18:33 -0300

    v4
    busqueda de átomos avanzado

--- perm_event/main.py
+++ perm_event/main.py
@@ -14,6 +14,60 @@ def open_dataset(filename):
     atoms_coordinates = fn['coordinates']
     return atoms_coordinates
 
+def pore_traject(atoms_coordinates, pore_radius = 6):
+    '''
+    Calcula la trayectoria del poro  
+       
+       Pre: atoms_coordinates: arreglo 3D de la forma (frames, atoms, xyz_coordinates)
+            pore_radius: un entero (radio del cilindro)
+       Pos: Devuelve un objeto (poro) a partir de los átomos de referencia.
+    '''
+
+    # Coordenadas de los átomos de referencia (3)
+    a_top = atoms_coordinates[:,0,2]
+    a_low = atoms_coordinates[:,2,2]
+    a1_xy = atoms_coordinates[:,1,0:2]
+    a2_xy = atoms_coordinates[:,2,0:2]
+
+    # Se llama a la clase pore y se determina la trayectoria del 
+    # poro a partir de las coordenadas de los átomos de referencia y el radio.
+
+    Pore = pore.Pore_cylinder_traj(a_top,a_low,a1_xy,a2_xy,pore_radius)      #cosa a mejorar: poder elegir 
+                                                                             #los átomos de referencia.
+                                                                        
+    return Pore
+
+def hacer_comparacion_un_solo_saque(atoms_coordinates, frame, atom_list,Pore):
+    '''Devuelve una lista de átomos que se encuentran dentro del cilindro
+    en un frame determinado
+    Pre:
+    atoms_coordinates: array 3D de la forma [frames, atoms, coordenadas_xyz]
+    frame: un entero
+    atom_list: una lista de enteros
+    Pore: un objeto Pore_cylinder_traj'''
+    pore_cylinder = Pore[frame]
+    atom_coord = atoms_coordinates[frame,atom_list,:]
+    distance_to_center = (((atom_coord[:,0:2] - pore_cylinder.xy_center)**2).sum(axis = 1))**(1/2)
+    condition_1 = distance_to_center < pore_cylinder.radius
+    condition_2 = (pore_cylinder.low < atom_coord[:,2]) & (atom_coord[:,2] < pore_cylinder.top)
+    true_list = np.where((condition_1 & condition_2) == True)
+    return atom_list[true_list]
+
+
+
+filename = 'trajectory.nc'
+with nc.Dataset(filename) as dataset:
+    atoms_coordinates = dataset['coordinates']
+
+Pore = pore_traject(atoms_coordinates)
+atom_list = [] #TODO
+
+compendio_atomos = []
+for n_frame, frame in enumerate(atoms_coordinates):
+    lista_true_atomos = hacer_comparacion_un_solo_saque(atoms_coordinates, n_frame, atom_list, Pore)
+    compendio_atomos.append(lista_true_atomos)
+
+
 
 n_eventos = 0
 
