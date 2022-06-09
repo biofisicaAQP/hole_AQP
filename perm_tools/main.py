3ce89efe2a6f163b1bc7e2633f81cf748c7e7653
Gera! <g.zerbetto@gmail.com>
2022-06-09 15:29:43 -0300

    v1.2
    main function updated

--- perm_tools/main.py
+++ perm_tools/main.py
@@ -1,1 +1,26 @@
-import pf_calculator.pf_calculator
+import pf_calculator.pf_calculator as pf_calc
+import os
+import matplotlib.pyplot as plt
+import pandas as pd
+
+molecule = os.path.dirname(os.getcwd()).split('/')[-1]
+dirs = next(os.walk('.'))[1]
+dirs.sort()
+chains = [0,1,2,3]
+chains_letter = ['A','B','C','D']
+(ref_z_1, ref_z_2, ref_xy_1, ref_xy_2) = (0,1,1,2)
+pf_dict_list = []
+for dir in dirs:
+    filename = os.path.join(dir, 'perm.nc')
+    for chain_id in chains:
+        (pf,plot) = pf_calc.pf_calculator(filename, chain_id, ref_z_1, ref_z_2, ref_xy_1, ref_xy_2)
+        plt.savefig(f'{dir}_{chains_letter[chain_id]}_msd.png')
+        plt.close()
+        print(f'{dir}_{chains_letter[chain_id]}_pf: {pf/1e-14:.4f}e-14')
+        pf_dict = {'Time':dir,'Molecule': molecule ,'Chain':chains_letter[chain_id], 'pf':pf}
+        pf_dict_list.append(pf_dict)
+
+pf_dataframe = pd.DataFrame(pf_dict_list)
+print(pf_dataframe)
+pf_dataframe.to_csv('pf_dataframe.csv')
+
--- perm_tools/pf_calculator/pf_calculator.py
+++ perm_tools/pf_calculator/pf_calculator.py
@@ -1,37 +1,43 @@
 from lib.classes import *
 from lib.functions import *
 import matplotlib.pyplot as plt
+def pf_calculator(filename, chain_id, ref_z_1_order, ref_z_2_order, ref_xy_1_order, ref_xy_2_order, pore_radius = 6, timestep = 1e-12):
+    n_ref_atoms = 3
+    first_non_ref_atom = 12
+    drop_msd_points = 10
+    vol_h2o = 2.99003322259e-23
+    ref_z_1_atom = ref_z_1_order + chain_id*n_ref_atoms
+    ref_z_2_atom = ref_z_2_order + chain_id*n_ref_atoms
+    ref_xy_1_atom = ref_xy_1_order + chain_id*n_ref_atoms
+    ref_xy_2_atom = ref_xy_2_order + chain_id*n_ref_atoms
 
+    data = open_dataset(filename)
+    atoms_coordinates = data['coordinates']
+    total_atoms = len(data.dimensions['atom'])
+    total_frames = len(data.dimensions['frame'])
+    top_atom, low_atom = get_z_top_low(atoms_coordinates, ref_z_1_atom, ref_z_2_atom)
+    
+    Pore = pore_traject(atoms_coordinates, top_atom, low_atom, ref_xy_1_atom, ref_xy_2_atom, pore_radius)
+    atom_list = list(range(first_non_ref_atom,total_atoms))
 
-filename = 'perm.nc'
-
-ref_z_1_atom = 0
-ref_z_2_atom = 1
-ref_xy_1_atom = 0
-ref_xy_2_atom = 2
-
-data = open_dataset(filename)
-atoms_coordinates = data['coordinates']
-total_atoms = len(data.dimensions['atom'])
-total_frames = len(data.dimensions['frame'])
-top_atom, low_atom = get_z_top_low(atoms_coordinates, ref_z_1_atom, ref_z_2_atom)
-first_non_ref_atom = 3
-Pore = pore_traject(atoms_coordinates, top_atom, low_atom, ref_xy_1_atom, ref_xy_2_atom)
-atom_list = list(range(first_non_ref_atom,total_atoms))
-
-compendio_atomos = atoms_inside_pore(atoms_coordinates, atom_list, Pore)
-coord_atoms_in_pore = atoms_coordinates[:,compendio_atomos,:]
-dz = coord_atoms_in_pore[1:,:,2] - coord_atoms_in_pore[:-1,:,2]
-dz_exp = np.expand_dims(dz, axis=2)
-atoms_dz_array = np.concatenate(
-    (coord_atoms_in_pore[1:,:,:],dz_exp),
-     axis=2)
-atoms_dz_array = drop_dz(coord_atoms_in_pore,atoms_dz_array, Pore)
-n_array = np.insert(compute_n(atoms_dz_array, Pore),0,0)
-n_fragments = 200
-n_msd = compute_msd(n_array, n_fragments)
-time_axis = np.arange(n_msd.size)
-plt.plot(time_axis, n_msd, 'bo')
-plt.show()
+    compendio_atomos = atoms_inside_pore(atoms_coordinates, atom_list, Pore)
+    coord_atoms_in_pore = atoms_coordinates[:,compendio_atomos,:]
+    dz = coord_atoms_in_pore[1:,:,2] - coord_atoms_in_pore[:-1,:,2]
+    dz = np.insert(dz, dz.shape[0],np.zeros(shape = dz.shape[1]), axis = 0)
+    dz_exp = np.expand_dims(dz, axis=2)
+    atoms_dz_array = np.concatenate(
+        (coord_atoms_in_pore,dz_exp),
+        axis=2)
+    atoms_dz_array = drop_dz(atoms_dz_array, Pore)
+    n_array = compute_n(atoms_dz_array, Pore)
+    fragments = 200
+    n_msd = compute_msd(n_array, fragments)
+    
+    
+    time_axis = np.arange(n_msd[drop_msd_points:].size)
+    pf = (regresion_lineal(time_axis, n_msd[drop_msd_points:]))*vol_h2o/(2*timestep)
+    #print(f'{pf/1e-14:.4f}e-14')
+    msd_plot = plt.plot(time_axis, n_msd[drop_msd_points:], 'bo')
+    return (pf, msd_plot)
 
 
