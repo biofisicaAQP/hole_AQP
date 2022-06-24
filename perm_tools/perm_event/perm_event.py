47b1dec4c9e18e1c0e1dc25aac13870f58415d49
Gera! <g.zerbetto@gmail.com>
2022-06-24 16:50:11 -0300

    perm event main created

--- perm_tools/perm_event/perm_event.py
+++ perm_tools/perm_event/perm_event.py
@@ -1,68 +1,51 @@
-import netCDF4 as nc
-import numpy as np
-from lib import classes
+from tkinter import N
+import pandas as pd
 from lib.functions import *
 
+def perm_event_calculator(filename, chain_id, ref_z_1_order, ref_z_2_order, ref_xy_1_order, ref_xy_2_order, chains_letter, pore_radius = 6):
+    n_ref_atoms = 3
+    first_non_ref_atom = 12
+    ref_z_1_atom = ref_z_1_order + chain_id*n_ref_atoms
+    ref_z_2_atom = ref_z_2_order + chain_id*n_ref_atoms
+    ref_xy_1_atom = ref_xy_1_order + chain_id*n_ref_atoms
+    ref_xy_2_atom = ref_xy_2_order + chain_id*n_ref_atoms
 
-filename = 'AOX_ref_atoms.nc'
-ref_z_1_atom = 0
-ref_z_2_atom = 1
-ref_xy_1_atom = 0
-ref_xy_2_atom = 2
-data = open_dataset(filename)
-atoms_coordinates = data['coordinates']
-total_atoms = len(data.dimensions['atom'])
-total_frames = len(data.dimensions['frame'])
-top_atom, low_atom = get_z_top_low(atoms_coordinates, ref_z_1_atom, ref_z_2_atom)
-first_non_ref_atom = 3
-Pore = pore_traject(atoms_coordinates, top_atom, low_atom, ref_xy_1_atom, ref_xy_2_atom)
-atom_list = list(range(first_non_ref_atom,total_atoms))
-n_eventos = 0
+    data = open_dataset(filename)
+    atoms_coordinates = data['coordinates']
+    total_atoms = len(data.dimensions['atom'])
+    total_frames = len(data.dimensions['frame'])
+    top_atom, low_atom = get_z_top_low(atoms_coordinates, ref_z_1_atom, ref_z_2_atom)
+    first_non_ref_atom = 12
+    Pore = pore_traject(atoms_coordinates, top_atom, low_atom, ref_xy_1_atom, ref_xy_2_atom, pore_radius)
+    atom_list = list(range(first_non_ref_atom,total_atoms))
+    n_eventos = 0
 
-compendio_atomos = atoms_inside_pore(atoms_coordinates, atom_list, Pore)
+    compendio_atomos = atoms_inside_pore(atoms_coordinates, atom_list, Pore)
+    list_events = []
+    for atom in compendio_atomos:
+        entro = False
+        for frame in range(1,total_frames):
+            if frame % 10000 == 0:
+                print('Atom:', atom, ' Frame:', frame)
+            is_in_cylinder_prev = is_in_cylinder(atoms_coordinates, frame-1,atom,Pore)
+            is_in_cylinder_now = is_in_cylinder(atoms_coordinates, frame, atom, Pore)
 
-for atom in compendio_atomos:
-    entro = False
-    for frame in range(1,total_frames):
-        if frame % 100 == 0:
-            print('Atom:', atom, ' Frame:', frame)
-        is_in_cylinder_prev = is_in_cylinder(atoms_coordinates, frame-1,atom,Pore)
-        is_in_cylinder_now = is_in_cylinder(atoms_coordinates, frame, atom, Pore)
+            if is_in_cylinder_now and is_in_cylinder_prev:
+                continue
 
-        if is_in_cylinder_now and is_in_cylinder_prev:
-            continue
-
-        if is_in_cylinder_now and not is_in_cylinder_prev:
-            ingreso = por_donde_pasa(atoms_coordinates, frame, top_atom, low_atom, atom)
-            entro = True
-        
-        if is_in_cylinder_prev and not is_in_cylinder_now and entro:
-            salida = por_donde_pasa(atoms_coordinates, frame, top_atom, low_atom, atom)
-            if ingreso != salida:
-                n_eventos += 1
-            print(n_eventos)
-            entro = False
-print(n_eventos)
-
-
-
-
-
-#n_eventos = 0
-
-#buscar átomos que pasan por el poro alguna vez: hacer lista atom_pore_list DONE
-
-#iterar por la lista de átomos... DONE
-    #iterar por cada frame, desde el 1... DONE
-        #evaluar si en el frame n-1 está dentro del cilindro(booleano) DONE
-        #evaluar si en el frame n está dentro del cilindro (booleano) DONE
-
-            #si en n-1 no estaba y en el n está dentro del cilindro: DONE
-            #evaluar si está más cerca del top o del bottom (ver por donde entró) DONE
-            #almacenar tag del átomo de entrada DONE
-
-            #si en n no está en el cilindro:
-            #evaluar si está más cerca del top o del bottom (ver por donde salió)
-            #almacenar tag del átomo de salida
-                #si tag_atom_salida != tag_atom_entrada:
-                #n_eventos += 1
+            if is_in_cylinder_now and not is_in_cylinder_prev:
+                ingreso = por_donde_pasa(atoms_coordinates, frame, top_atom, low_atom, atom)
+                entro = True
+                frame_entrada = frame
+            
+            if is_in_cylinder_prev and not is_in_cylinder_now and entro:
+                salida = por_donde_pasa(atoms_coordinates, frame, top_atom, low_atom, atom)
+                if ingreso != salida:
+                    n_eventos += 1
+                    frame_salida = frame
+                    event_dict = {'Chain':chains_letter[chain_id],'Atom': atom, 'Start':frame_entrada, 'End': frame_salida}
+                    list_events.append(event_dict)
+                    print(n_eventos)
+                entro = False
+    print(n_eventos)
+    return(list_events, n_eventos)
---
+++ perm_tools/perm_events_main.py
@@ -0,0 +1,14 @@
+import perm_event.perm_event as perm_ev_calc
+import pandas as pd
+filename = 'perm.nc'
+chains = [0,1,2,3]
+chains_letter = ['A','C','B','D']
+(ref_z_1, ref_z_2, ref_xy_1, ref_xy_2) = (0,1,1,2)
+list_events_total = []
+for chain_id in chains:
+    (list_events_chain, n_eventos) = perm_ev_calc.perm_event_calculator(filename, chain_id, ref_z_1, ref_z_2, ref_xy_1, ref_xy_2, chains_letter)
+    print(f'Chain {chains_letter[chain_id]} perm events: {n_eventos}')
+    list_events_total += list_events_chain
+
+event_df = pd.DataFrame(list_events_total)
+event_df.to_csv('event_df.csv')
